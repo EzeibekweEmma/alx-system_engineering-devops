2d83fd3cfd7057e6d26816268a25a6a2ffd47f8e
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-04-22 01:58:47 +0100

    Create a file Using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file named school in tmp with owner and group www-data
+
+file {'/tmp/school':
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+  mode    => '0744',
+}
+
