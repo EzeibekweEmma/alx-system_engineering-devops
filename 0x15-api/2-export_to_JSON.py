d0aa962dddff37338fb1ee77aa04a91c561c9222
Ezeibewke Emmanuel <ezeibekweemma@gmail.com>
2023-05-26 04:15:05 +0100

    fix W291 trailing whitespace

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Using what you did in the task #0, extend your Python script to export data 
+"""Using what you did in the task #0, extend your Python script to export data
 in the CSV format.
 """
 
