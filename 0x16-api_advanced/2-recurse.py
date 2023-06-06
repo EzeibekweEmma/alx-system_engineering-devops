fd5803ee2ac9837bd38bb79d49a91d7320c13362
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2023-06-06 22:43:50 +0100

    fix error max of 80 charater

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-recursive function that queries the Reddit API and returns a list containing the titles of
-all hot articles for a given subreddit. If no results are found for the given subreddi
-the function should return None.
+recursive function that queries the Reddit API and
+returns a list containing the titles of all hot
+articles for a given subreddit
 """
 
 import requests
